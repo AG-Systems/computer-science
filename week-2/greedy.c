6c0d78f99213ea20f2d356211534096a6cd6617a
Auriga <AG-Systems@users.noreply.github.com>
2015-12-20 23:39:20 -0800

    Update greedy.c

--- week-2/greedy.c
+++ week-2/greedy.c
@@ -1,15 +1,44 @@
 #include <stdio.h>
+#include <cmath>
+#include <Windows.h>
 
 int main(void)
 {
 	float change;
-	int coins;
+	int coins = 0;
+	int stage1;
+	/*
 	float quator = 0.25;
 	float dime = 0.10;
 	float nickel = 0.5;
 	float penny = 0.1;
+	*/
+	int quator = 25;
+	int dime = 10;
+	int nickel = 5;
+	int penny = 1;
 	scanf("%d", &change);
-
-
-
+	int convert = change * 100;
+	while (convert / quator)
+	{
+		coins++;
+		int stage1 = convert - quator;
+	}
+	while (convert / dime)
+	{
+		coins++;
+		int stage1 = convert - dime;
+	}
+	while (convert / nickel)
+	{
+		coins++;
+		int stage1 = convert - nickel;
+	}
+	while( convert / penny)
+	{
+		coins++;
+		int stage1 = convert - penny;
+	}
+	printf("%d", coins);
+	system("PAUSE");
 }
