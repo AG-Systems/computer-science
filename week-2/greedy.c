c9774db0dc8052429f56400ff7a6a99abbf7512a
Auriga <AG-Systems@users.noreply.github.com>
2015-12-20 18:57:43 -0800

    Update greedy.c

--- week-2/greedy.c
+++ week-2/greedy.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(void)
+{
+	float change;
+	int coins;
+	float quator = 0.25;
+	float dime = 0.10;
+	float nickel = 0.5;
+	float penny = 0.1;
+	scanf("%d", &change);
+
+
+
+}
