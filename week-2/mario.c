38f28093e2877a10f570118747b3c42db385d131
Auriga <AG-Systems@users.noreply.github.com>
2015-12-20 18:37:59 -0800

    Create mario.c

---
+++ week-2/mario.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main(void)
+{
+	int height;
+	int hash = 0;
+	int counter = 0;
+	scanf("%d", &height);
+	int space = height - 1;
+	for(int x = 0; x < (space); x++)
+	{
+		counter++;
+		if(x = 0)
+		{
+			hash = 2;
+		}
+		if(x < 1)
+		{
+			hash++;
+		}
+		space--;
+		if(counter > 1)
+		{
+			for(int z = 0; z < hash; z++)
+			{
+				printf("#");
+
+			}
+		printf("\n");
+		}
+	}
+	system("PAUSE");
+}
