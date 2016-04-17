d6e376cd7c29731d909f3eb7055c9a640f3e2c72
Auriga <AG-Systems@users.noreply.github.com>
2016-04-16 17:32:21 -0700

    Update GameOfFifteen.c

--- cs50x/week-3/GameOfFifteen.c
+++ cs50x/week-3/GameOfFifteen.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int array[4][4] = 
 {  
@@ -8,15 +9,41 @@ int array[4][4] =
    {13,14,15, 16 }   
 };
 
+void swap(int array[4][4], int indexA, int indexB)
+{
+    for (size_t i=0;sizeof(array[0])/sizeof(array[0][0]);++i)
+    {
+        int temp = array[indexA][i];
+        array[indexA][i] = array[indexB][i];
+        array[indexB][i] = temp;
+    }
+}
 void playgame()
 {
 	int x = 0;
 	int y = 0;
 	printf("What piece do you want to move for the x and y: ");
-	scanf("%s",&x,&y);
-	printf("%d", &x);
+	scanf("%s",&x);
+	scanf("%s",&y);
+	if(array[x][y+1] == 16 || array[x+1][y] == 16 || array[x+1][y+1] == 16)
+	{
+		swap(array[x][y],x,y);		
+	}
 };
+
+void shuffle(int *a, int len)
+{
+    int i;
+    for (i = len - 1; i > 0; i--)   /* note that we traverse the array backwards */
+    {
+        int r = rand() % (i + 1);   /* random value from 0 to i, inclusive */
+        int t = a[i];               /* swap a[i] and a[r] */
+        a[i] = a[r];
+        a[r] = t;
+    }
+}
 int main()
 {
+	
 	playgame();		
 }
