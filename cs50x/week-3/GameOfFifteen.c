cb3aeba4145097be6ada4150159c8b846b48487b
Auriga <AG-Systems@users.noreply.github.com>
2016-04-16 21:15:31 -0700

    Update GameOfFifteen.c
    I did it in c++. I'm honesty really sick of using C.

--- cs50x/week-3/GameOfFifteen.c
+++ cs50x/week-3/GameOfFifteen.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <algorithm>
+#include <ctime>
 
-int array[4][4] = 
+#define SWAP(x, y, T) do { T SWAP = x; x = y; y = SWAP; } while (0);
+
+int a[4][4] = 
 {  
    {1, 2, 3, 4} ,   
    {5, 6, 7, 8} ,   
    {9, 10, 11, 12} ,
-   {13,14,15, 16 }   
+   {13,14,15, 0 }   
 };
 
-void swap(int array[4][4], int indexA, int indexB)
-{
-    for (size_t i=0;sizeof(array[0])/sizeof(array[0][0]);++i)
-    {
-        int temp = array[indexA][i];
-        array[indexA][i] = array[indexB][i];
-        array[indexB][i] = temp;
-    }
-}
+
 void playgame()
 {
-	int x = 0;
-	int y = 0;
-	printf("What piece do you want to move for the x and y: ");
-	scanf("%s",&x);
-	scanf("%s",&y);
-	if(array[x][y+1] == 16 || array[x+1][y] == 16 || array[x+1][y+1] == 16)
+	while(true)
 	{
-		swap(array[x][y],x,y);		
+		int x = 0;
+		int y = 0;
+		for(int x = 0; x < 4; x++)
+		{
+			for(int z = 0; z < 4; z++)
+			{
+				printf("%d  ",a[x][z]);
+
+			}
+			printf("\n");
+		}
+		printf("What piece do you want to move for the x and y: ");
+		scanf("%s",&x);
+		scanf("%s",&y);
+		if(a[x][y] != 00 )
+		{
+			if(a[x+1][y] == 00)
+			{
+				SWAP(a[x][y], a[x+1][y],int);
+			}
+			else if(a[x][y+1] == 00)
+			{
+				SWAP(a[x][y], a[x][y+1],int);			
+			}
+			else if(a[x][y-1] == 00)
+			{
+				SWAP(a[x][y], a[x][y-1],int);					
+			}
+			else if(a[x-1][y] == 00)
+			{
+				SWAP(a[x][y], a[x-1][y],int);			
+			}
+		}
 	}
 };
 
@@ -44,6 +67,6 @@ void shuffle(int *a, int len)
 }
 int main()
 {
-	
+	// shuffle(a,16);
 	playgame();		
 }
