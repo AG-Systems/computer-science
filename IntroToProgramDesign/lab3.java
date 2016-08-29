a9b59dfbb95dc7eab815b4dcff0e17d6e7bb6594
Auriga <AG-Systems@users.noreply.github.com>
2016-08-29 12:48:31 -0700

    Create lab3.java

---
+++ IntroToProgramDesign/lab3.java
@@ -0,0 +1,38 @@
+// Max
+
+//CIS254 Lab 4
+
+//This program will… (describe what it does in 1 or 2 lines)
+
+import java.util.Scanner;
+
+public class lab3
+{
+  public static int totaltickets(int adult_tickets, int child_tickets)
+  {
+	  int totaltickets = adult_tickets + child_tickets;
+	  System.out.println("The total amount of tickets are: " + totaltickets);
+	  return totaltickets;
+  }
+  public static double ticketsales(int adult_tickets, int child_tickets)
+  {
+	  int childsales = child_tickets * 5;
+	  int adultsales = adult_tickets * 9;
+	  double ticketsales = adultsales + childsales;
+	  System.out.println("The total price is: $" + ticketsales);
+	  return ticketsales;
+  }
+  public static void main(String[] args)
+  {
+	  int adult_tickets;
+	  int child_tickets;
+	  Scanner input = new Scanner(System.in);
+	  System.out.print("How many adult tickets?");
+	  adult_tickets = input.nextInt();
+	  System.out.print("How many child tickets?");
+	  child_tickets = input.nextInt();	
+	  ticketsales(adult_tickets,child_tickets);
+	  totaltickets(adult_tickets,child_tickets);
+	  System.out.println("Made By Max");	  
+  }
+}
