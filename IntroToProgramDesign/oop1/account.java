33928db1817189d7d767c01110733c81872c0abc
Auriga <AG-Systems@users.noreply.github.com>
2016-11-07 12:35:33 -0800

    Create account.java

---
+++ IntroToProgramDesign/oop1/account.java
@@ -0,0 +1,17 @@
+package oop1;
+
+public class account 
+{
+	private String name;
+	
+	//need public methods to access private data
+	
+	public void setName(String aname)
+	{
+		name = aname;
+	}
+	public String getName()
+	{
+		return name;
+	}
+}
