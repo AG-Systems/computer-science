e7bf1ae03801859929c8fbc15f4832a483a81489
Auriga <AG-Systems@users.noreply.github.com>
2016-11-07 12:35:44 -0800

    Create accounttest.java

---
+++ IntroToProgramDesign/oop1/accounttest.java
@@ -0,0 +1,13 @@
+package oop1;
+
+public class accounttest {
+
+	public static void main(String[] args) 
+	{
+		// create an object
+		account myaccount = new account();
+		myaccount.setName("Max");
+		System.out.print("My name is " + myaccount.getName());
+	}
+
+}
