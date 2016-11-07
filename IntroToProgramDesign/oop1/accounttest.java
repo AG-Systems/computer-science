19e7976543a8b818f2dab9f22916a01966bd7396
Auriga <AG-Systems@users.noreply.github.com>
2016-11-07 13:08:00 -0800

    Update accounttest.java

--- IntroToProgramDesign/oop1/accounttest.java
+++ IntroToProgramDesign/oop1/accounttest.java
@@ -1,13 +1,28 @@
 package oop1;
 
+import java.util.Scanner;
+
 public class accounttest {
 
 	public static void main(String[] args) 
 	{
 		// create an object
+		String myname;
+		double balance;
+		Scanner input = new Scanner(System.in);
 		account myaccount = new account();
-		myaccount.setName("Max");
-		System.out.print("My name is " + myaccount.getName());
+		System.out.println("Enter name: ");
+		myname = input.nextLine();
+		myaccount.setName(myname);
+		System.out.println("Enter balance: ");
+		balance = input.nextDouble();
+		myaccount.deposit(balance);
+		System.out.println("My name is " + myaccount.getName());
+		System.out.println("Balance is " + myaccount.getBalance());
+		myaccount.deposit(100.0);
+		System.out.println("Balance is " + myaccount.getBalance());
+		myaccount.withdraw(100.0);
+		System.out.println("Balance is " + myaccount.getBalance());
 	}
 
 }
