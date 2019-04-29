da23703490c3d7ea875ac69b993b845c2814b857
Auriga <AG-Systems@users.noreply.github.com>
2019-04-29 14:44:36 -0700

    Update game.cpp

--- cs_2b/Assignment-3:-Pointers-and-Dynamic-Memory/game.cpp
+++ cs_2b/Assignment-3:-Pointers-and-Dynamic-Memory/game.cpp
@@ -2,6 +2,23 @@
 #include <string>
 
 
+/*
+Name: Max Chakhmatov
+Date: April 29 2019
+Instructor:  Dave Harden
+Name of file: a3_2.cpp
+Description:  Asks the user how many scores the user wants to input, then sorts the 
+scores in descending order and displays the scores.
+Output: Displays the scores in descending order.
+*/
+
+
+/*
+Intalializes the array using recurssion.
+The reason why initialize array recursion function is called
+in the top so the array initialize the array in asceending order
+( The prompts are asked in order: #1, #2, #3 etc)
+*/
 void initializeArrays(std::string names[], int scores[], int size)
 {
     if(size > 0)
@@ -18,6 +35,16 @@ void initializeArrays(std::string names[], int scores[], int size)
     }
 }
 
+
+
+/*
+Sorts the data by using bubble sort with recurssion.
+Does it in descending order
+Each recurssion call, loops through it once and swaps values
+in the right order then recalls it until the base case is reached.
+
+With recursion, the function is O(N^2) for time complexity
+*/
 void sortData(std::string names[], int scores[], int size)
 {
     if (size <= 1)
@@ -42,6 +69,16 @@ void sortData(std::string names[], int scores[], int size)
 }
 
 
+/* 
+Displays the data in descending order by using recursion.
+The recursion function is called first so the data can be printed
+in descending order. 
+
+Prints the top scorers statement first then prints out the array elements 
+from top to bottom.
+
+Uses recursion to loop through the array.
+*/
 void displayData(const std::string names[], const int scores[], int size)
 {
     if(size > 0)
