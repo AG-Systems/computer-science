cf54ddce1ee2ca3e1b845145891050ba10fbbec6
Auriga <AG-Systems@users.noreply.github.com>
2019-05-02 18:04:21 -0700

    Update mystring.cpp

--- cs_2b/Assignment-4:-Classes-with-Dynamic-Memory-1/mystring.cpp
+++ cs_2b/Assignment-4:-Classes-with-Dynamic-Memory-1/mystring.cpp
@@ -7,25 +7,69 @@
 
 namespace cs_mystring {
 
-    MyString::MyString(char* new_string)
+    MyString::MyString(const char* new_string):string(nullptr) 
     {        
-        if(strlen(new_string) > 0)
+        size_t string_length = strlen(new_string);
+        /*
+        if(string_length > 0)
         {
+            string = new char[string_length + 1];
             strcpy (string, new_string);
         } else {
-            strcpy (string, "\0");
+            string = new char[1];
+            string[0] = '\0';
         }
+        */
+        string = new char[string_length + 1];
+        strcpy (string, new_string);
         
     }
 
-    MyString::MyString()
+
+    MyString::MyString(const MyString& right):string(nullptr) 
+    {     
+        size_t string_length = strlen(right.string);
+        #if 0
+        if(string_length > 0)
+        {
+            /* nice*/
+            string = new char[string_length + 1];
+            strcpy (string, right.string);
+        } else {
+            string = new char[1];
+            string[0] = '\0';
+        }  
+        #endif
+        
+        /*
+        string = new char[string_length + 1];
+        strcpy (string, right.string);
+        */
+
+        assignment(right.string);     
+    }
+
+    void MyString::assignment(const char* ptr)
+    {
+        delete []string;
+        size_t string_length = strlen(ptr);
+        string = new char[string_length + 1];
+        strcpy (string, ptr);      
+    }
+
+    MyString::MyString():string(nullptr) 
     {   
-        strcpy (string, "\0");
+        /*
+        string = new char[1];
+        string[0] = '\0';
+        */
+        assignment("");
     }
 
     MyString::~MyString() 
     { 
         delete []string; 
+        string = NULL;
     } 
 
     int MyString::length() const
@@ -34,74 +78,91 @@ namespace cs_mystring {
     }
 
     
-    char& MyString::operator[](int i)
+    char& MyString::operator[](size_t i)
     {
+        assert(i < strlen(string));
         return string[i];
     }
     
 
-    char MyString::operator[](unsigned int i) const
+    const char& MyString::operator[](size_t i) const
     {
-        char val;
-        val = string[i];
-        return val;
+        assert(i < strlen(string));
+        return string[i];
     }
 
+    MyString& MyString::operator=(const MyString& right)
+    {
+        assignment(right.string);
+        /*
+        char * temp = new char[0];
+        return temp;
+        */
+       return *this;
+
+    }
+
+    MyString& MyString::operator=(const char* right)
+    {
+       assignment(right);
+       return *this;    
+    }
+    
 
     bool operator<(const MyString& left, const MyString& right)
     {
-        return (strcmp(left.string, right.string) > 0);
+        return (strcmp(left.string, right.string) < 0);
     }
     bool operator<(const MyString& left, const char * right)
     {
-        return (strcmp(left.string, right) > 0);
+        return (strcmp(left.string, right) < 0);
     }
     bool operator<(const char * left, const MyString& right)
     {
-        return (strcmp(left, right.string) > 0);
+        return (strcmp(left, right.string) < 0);
     }
 
 
 
     bool operator<=(const MyString& left, const MyString& right)
     {
-        return (strcmp(left.string, right.string) >= 0);
+        return (strcmp(left.string, right.string) <= 0);
     }
     bool operator<=(const MyString& left, const char * right)
     {
-        return (strcmp(left.string, right) >= 0);
+        return (strcmp(left.string, right) <= 0);
     }
     bool operator<=(const char * left, const MyString& right)
     {
-        return (strcmp(left, right.string) >= 0);
+        return (strcmp(left, right.string) <= 0);
     }
 
 
     bool operator>(const MyString& left, const MyString& right)
     {
-        return (strcmp(left.string, right.string) <= 0);
+        return (strcmp(left.string, right.string) > 0);
     }
     bool operator>(const MyString& left, const char * right)
     {
-        return (strcmp(left.string, right) <= 0);
+        return (strcmp(left.string, right) > 0);
     }
     bool operator>(const char * left, const MyString& right)
     {
-        return (strcmp(left, right.string) <= 0);
+        return (strcmp(left, right.string) > 0);
     }
 
 
     bool operator>=(const MyString& left, const MyString& right)
     {
-        return (strcmp(left.string, right.string) <= 0);
+        return (strcmp(left.string, right.string) >= 0);
     }
     bool operator>=(const MyString& left, const char * right)
     {
-        return (strcmp(left.string, right) <= 0);
+        return (strcmp(left.string, right) >= 0);
     }
     bool operator>=(const char * left, const MyString& right)
     {
-        return (strcmp(left, right.string) <= 0);
+        return (strcmp(left, right.string) >= 0);
     }
 
 
