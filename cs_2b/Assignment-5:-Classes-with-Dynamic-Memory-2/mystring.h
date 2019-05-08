c106772476df0d75bbb9e9c3d1da6731223a71be
Auriga <AG-Systems@users.noreply.github.com>
2019-05-08 16:36:22 -0700

    Create mystring.h

---
+++ cs_2b/Assignment-5:-Classes-with-Dynamic-Memory-2/mystring.h
@@ -0,0 +1,79 @@
+#ifndef MYSTRING_H
+#define MYSTRING_H
+
+
+#include <iostream>
+#include <string>
+#include <cassert>
+#include <string>
+#include <cmath> 
+#include <iostream>
+
+
+using namespace std;
+
+namespace cs_mystring {
+
+   class MyString
+   { 
+      public:
+        MyString(const char* new_string);  
+        MyString(const MyString& right);  
+        MyString();  
+        ~MyString();
+        
+        int length() const;
+        
+        friend bool operator<(const MyString& left, const MyString& right);
+        friend bool operator<(const MyString& left, const char * right);
+        friend bool operator<(const char * left, const MyString& right);
+
+        friend bool operator<=(const MyString& left, const MyString& right);
+        friend bool operator<=(const MyString& left, const char * right);
+        friend bool operator<=(const char * left, const MyString& right);
+
+        friend bool operator>(const MyString& left, const MyString& right);
+        friend bool operator>(const MyString& left, const char right[]);
+        friend bool operator>(const char * left, const MyString& right);
+
+        friend bool operator>=(const MyString& left, const MyString& right);
+        friend bool operator>=(const MyString& left, const char * right);
+        friend bool operator>=(const char * left, const MyString& right);
+
+        friend bool operator==(const MyString& left, const MyString& right);
+        friend bool operator==(const MyString& left, const char * right);
+        friend bool operator==(const char * left, const MyString& right);
+
+        friend bool operator!=(const MyString& left, const MyString& right);
+        friend bool operator!=(const MyString& left, const char * right);
+        friend bool operator!=(const char * left, const MyString& right);
+
+        friend std::ostream& operator<<(std::ostream& out, 
+                            const MyString& right);
+
+        friend std::istream& operator>>(std::istream& in, 
+                                       MyString& right);
+        char& operator[](size_t i);
+        const char& operator[](size_t i) const;
+
+        MyString& operator=(const MyString& right);
+        MyString& operator=(const char * right);
+
+        friend MyString& operator+(const MyString& left, const MyString& right);
+        friend MyString& operator+(const MyString& left, const char * right);
+        friend MyString& operator+(const char * left, const MyString& right);
+
+        MyString& operator+=(const MyString& right);
+        MyString& operator+=(const char * right);
+
+        void read(std::ifstream& infile, const char right);
+        
+        static const int MAX_INPUT_SIZE = 127;
+
+      private:
+        void assignment(const char* ptr);
+        char* string;
+   }; 
+
+}
+#endif
