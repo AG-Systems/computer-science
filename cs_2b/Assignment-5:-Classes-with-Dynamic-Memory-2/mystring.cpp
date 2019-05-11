8a47c3f661ba5ee984eafd6e532f1d09207d3fed
Auriga <AG-Systems@users.noreply.github.com>
2019-05-10 23:35:58 -0700

    Update mystring.cpp

--- cs_2b/Assignment-5:-Classes-with-Dynamic-Memory-2/mystring.cpp
+++ cs_2b/Assignment-5:-Classes-with-Dynamic-Memory-2/mystring.cpp
@@ -7,6 +7,7 @@
 
 namespace cs_mystring {
 
+
     MyString::MyString(const char* new_string):string(nullptr) 
     {        
         size_t string_length = strlen(new_string);
@@ -16,10 +17,17 @@ namespace cs_mystring {
     }
 
 
+
+
+
     MyString::MyString(const MyString& right):string(nullptr) 
     {     
         assignment(right.string);     
     }
+
+
+
+
 
     void MyString::assignment(const char* ptr)
     {
@@ -29,10 +37,17 @@ namespace cs_mystring {
         strcpy (string, ptr);      
     }
 
+
+
+
     MyString::MyString():string(nullptr) 
     {   
         assignment("");
     }
+
+
+
+
 
     MyString::~MyString() 
     { 
@@ -40,12 +55,16 @@ namespace cs_mystring {
         string = NULL;
     } 
 
+
+
     int MyString::length() const
     {
         return strlen(string);
     }
 
     
+
+
     char& MyString::operator[](size_t i)
     {
         assert(i < strlen(string));
@@ -53,11 +72,17 @@ namespace cs_mystring {
     }
     
 
+
+
+
     const char& MyString::operator[](size_t i) const
     {
         assert(i < strlen(string));
         return string[i];
     }
+
+
+
 
     MyString& MyString::operator=(const MyString& right)
     {
@@ -66,6 +91,9 @@ namespace cs_mystring {
 
     }
 
+
+
+
     MyString& MyString::operator=(const char* right)
     {
        assignment(right);
@@ -73,14 +101,27 @@ namespace cs_mystring {
     }
     
 
+
+
     bool operator<(const MyString& left, const MyString& right)
     {
         return (strcmp(left.string, right.string) < 0);
     }
+
+
+
+
+
     bool operator<(const MyString& left, const char * right)
     {
         return (strcmp(left.string, right) < 0);
     }
+
+
+
+
+
+
     bool operator<(const char * left, const MyString& right)
     {
         return (strcmp(left, right.string) < 0);
@@ -92,10 +133,21 @@ namespace cs_mystring {
     {
         return (strcmp(left.string, right.string) <= 0);
     }
+
+
+
+
+
+
     bool operator<=(const MyString& left, const char * right)
     {
         return (strcmp(left.string, right) <= 0);
     }
+
+
+
+
+
     bool operator<=(const char * left, const MyString& right)
     {
         return (strcmp(left, right.string) <= 0);
@@ -106,24 +158,45 @@ namespace cs_mystring {
     {
         return (strcmp(left.string, right.string) > 0);
     }
+
+
+
+
+
     bool operator>(const MyString& left, const char * right)
     {
         return (strcmp(left.string, right) > 0);
     }
+
+
+
+
+
     bool operator>(const char * left, const MyString& right)
     {
         return (strcmp(left, right.string) > 0);
     }
 
 
+
+
     bool operator>=(const MyString& left, const MyString& right)
     {
         return (strcmp(left.string, right.string) >= 0);
     }
+
+
+
+
     bool operator>=(const MyString& left, const char * right)
     {
         return (strcmp(left.string, right) >= 0);
     }
+
+
+
+
+
     bool operator>=(const char * left, const MyString& right)
     {
         return (strcmp(left, right.string) >= 0);
@@ -134,28 +207,52 @@ namespace cs_mystring {
     {
         return (strcmp(left.string, right.string) == 0);
     }
+
+
+
+
+
     bool operator==(const MyString& left, const char * right)
     {
         return (strcmp(left.string, right) == 0);
     }
+
+
+
+
+
     bool operator==(const char * left, const MyString& right)
     {
         return (strcmp(left, right.string) == 0);
     }
 
 
+
+
     bool operator!=(const MyString& left, const MyString& right)
     {
         return (strcmp(left.string, right.string) != 0);
     }
+
+
+
+
     bool operator!=(const MyString& left, const char * right)
     {
         return (strcmp(left.string, right) != 0);
     }
+
+
+
+
     bool operator!=(const char * left, const MyString& right)
     {
         return (strcmp(left, right.string) != 0);
     }
+
+
+
+
 
     MyString operator+(const MyString& left, const MyString& right)
     {
@@ -168,6 +265,10 @@ namespace cs_mystring {
         return result;
     }
 
+
+
+
+
     MyString operator+(const MyString& left, const char * right)
     {
         MyString result;
@@ -178,6 +279,10 @@ namespace cs_mystring {
         strcat(result.string, right);
         return result;
     }
+
+
+
+
 
     MyString operator+(const char * left, const MyString& right)
     {
@@ -190,10 +295,18 @@ namespace cs_mystring {
         return result;
     }
 
+
+
+
+
     MyString& MyString::operator+=(const MyString& right)
     {
         *this = *this + right.string;    
         return *this;
+
+
+
+
     }
 
     MyString& MyString::operator+=(const char * right)
@@ -202,6 +315,9 @@ namespace cs_mystring {
         return *this;
     }
 
+
+
+
    ostream& operator<<(ostream& out, const MyString& right)
    {  
         
@@ -209,32 +325,31 @@ namespace cs_mystring {
       return out;
    }    
 
+
+
+
+
    istream& operator>>(istream& in, MyString& right)
-   {  
-        while (isspace(in.peek())){
-            in.ignore();
-        }
+   {    
         char temp[MyString::MAX_INPUT_SIZE + 1];
-        in.getline(temp, MyString::MAX_INPUT_SIZE);
+        in.getline(temp, MyString::MAX_INPUT_SIZE, ' ');
         delete [] right.string;
         right.string = new char[strlen(temp) + 1];
         strcpy(right.string, temp);
         in >> right.string;
-        return in;
-
-       
+        return in;  
    } 
+
+
+
 
    void MyString::read(std::istream& infile, const char right)
    {
-        
         char temp[MyString::MAX_INPUT_SIZE + 1];
         infile.getline(temp, MyString::MAX_INPUT_SIZE, right);
-        char * str = new char[strlen(temp)];
-        strcpy(str, temp);
-        
-        infile >> str;       
-
+        delete []string;
+        string = new char[strlen(temp) + 1];
+        strcpy (string, temp); 
    }
 
 
