8386d0628d93d5402a3053735017ca4ce2f87d93
Auriga <AG-Systems@users.noreply.github.com>
2019-06-04 18:50:24 -0700

    Create client.cpp

---
+++ cs_2b/Assignment-9:-Linked-Lists-1/client.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include "sequence.h"
+
+using namespace std;
+using namespace cs_sequence;
+
+int main() {
+	Sequence s;
+
+	for (int i = 0; i < 6; i++) 
+    {
+		s.insert(i);
+	}
+	
+	for (s.start(); s.is_item(); s.advance()) 
+    {
+		cout << s.current() << " ";
+	}
+	
+	cout << endl;
+}
