d46c1be92fc964517d5a995e5ed8a9c3b0a69d3a
Auriga <AG-Systems@users.noreply.github.com>
2019-10-02 12:46:10 -0700

    Update lab1_e1.asm

--- cs_10/lab1_e1.asm
+++ cs_10/lab1_e1.asm
@@ -6,7 +6,7 @@ syscall
 
 li $v0,8 #take in input
 la $a0, buffer #load byte space into address
-li $a1, 50 # allot the byte space for string
+li $a1, 50 # allocate 50 bits for address 1
 move $t0,$a0 #save string to t0
 syscall
 # opens prompt for user input
@@ -32,18 +32,19 @@ li $v0, 4
 syscall
 
 
-li $v0, 4       # you can call it your way as well with addi 
-la $a0, newline       # load address of the string
+li $v0,4 # print an integer
+move $a0,$t0 # integer to print goes in a0
 syscall
 
 
-addi $t3,$t1,4 
-li $v0,4 
-la $a0, result_age_string_1 
+
+addi $t3,$t1,4 # Adds the constant 4 to $t1 register and then saves it to t3
+li $v0,4 # load service number 4
+la $a0, result_age_string_1  #this will print "You will be "
 syscall
 
-li $v0,1 # print an integer
-move $a0,$t3 # integer to print goes in a0
+li $v0,1 # Prints your age + 4 from earlier
+move $a0,$t3
 syscall
 
 li $v0,4 #load service number for string print in $v0
@@ -51,7 +52,7 @@ la $a0, result_age_string_2 # load address of string to be printed into $a0
 syscall
 
 li $v0,10 #load service number in $v0
-syscall 
+syscall #This ends the program
 # the null-terminated string must be defined in data segment
 
 .data
